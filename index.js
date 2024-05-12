3a28967536cf0d937e88cf9ae39707cf3a8d87b1
Sanga <paolo.sangalli1@studenti.unimi.it>
2024-05-12 15:32:36 +0200

    minor changes v2 index.js

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 import { Octokit } from "octokit";
-import auth_key from './key.json' assert {type: "json"};
+import auth_key from "./key.json" assert { type: "json" };
 
 const octokit = new Octokit({
-    auth: auth_key.git_key,
+  auth: auth_key.git_key,
 });
 
 //request users
-var users = await octokit.request('GET /users', {
-    per_page: 100,
-    headers: {
-    'X-GitHub-Api-Version': '2022-11-28'
-    }
-})
+var users = await octokit.request("GET /users", {
+  per_page: 100,
+  headers: {
+    "X-GitHub-Api-Version": "2022-11-28",
+  },
+});
 
 //console.log(users.data[1])
 
-var repo = await octokit.request('GET /users/{username}/repos', {
-    username: 'mojombo',
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-})
+var repo = await octokit.request("GET /users/{username}/repos", {
+  username: "mojombo",
+  headers: {
+    "X-GitHub-Api-Version": "2022-11-28",
+  },
+});
 
-
-console.log(userRepo.data)
+console.log(repo.data);
