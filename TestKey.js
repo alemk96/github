0bc3be039d38f145c5869e704c3ab05ae163eb2e
alemk96 <mkmedda@gmail.com>
2024-05-15 16:43:03 +0200

    general code refractor

---
+++ TestKey.js
@@ -0,0 +1,6 @@
+
+const TestKey = {
+    git_key:"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"
+}
+
+export default TestKey;
--- index.js
+++ index.js
@@ -1,25 +1,56 @@
 import { Octokit } from "octokit";
-import auth_key from "./key.json" assert { type: "json" };
+//import * as authKey from "./key.json" assert { type: "json" };
+import TestKey from "./TestKey.js";
+
 
 const octokit = new Octokit({
-  auth: auth_key.git_key,
+  auth: TestKey.git_key, // Utilizza la chiave di autenticazione
 });
 
-//request users
-var users = await octokit.request("GET /users", {
-  per_page: 100,
-  headers: {
-    "X-GitHub-Api-Version": "2022-11-28",
-  },
-});
+const githubApiVersion = "2022-11-28"; // Versione API GitHub
 
-//console.log(users.data[1])
+/**
+* Esempi di richiesta all'API di GitHub
+*/
+try {
+ 
+  /**
+   * Permette di ottenere l'elenco degli utenti della piattaforma
+   * @returns {Promise<Response>}
+   */
+  const usersResponse = await octokit.request("GET /users", {
+    per_page: 100,
+    headers: {
+      "X-GitHub-Api-Version": githubApiVersion,
+    },
+  });
 
-var repo = await octokit.request("GET /users/{username}/repos", {
-  username: "mojombo",
-  headers: {
-    "X-GitHub-Api-Version": "2022-11-28",
-  },
-});
+  console.log("Elenco degli utenti:", usersResponse.data[0]);
+  
+  // Ottieni il nome del primo utente dall'elenco
+  const username = usersResponse.data[0].login;
 
-console.log(repo.data);
+  // Ottieni elenco dei repository per l'utente specificato
+  const reposResponse = await octokit.request(`GET /users/${username}/repos`, {
+    headers: {
+      "X-GitHub-Api-Version": githubApiVersion,
+    },
+  });
+
+  console.log("Elenco dei repository dell'utente:", reposResponse.data);
+
+  // Ottieni elenco delle issues per un repository specifico
+  const issuesResponse = await octokit.request(`GET /repos/${username}/god/issues`, {
+    headers: {
+      "X-GitHub-Api-Version": githubApiVersion,
+    },
+  });
+
+  console.log("Elenco delle issues nel repository:", issuesResponse.data[0]);
+
+  
+} catch (error) {
+    console.error("Errore durante la richiesta all'API di GitHub:", error);
+}
+
+
